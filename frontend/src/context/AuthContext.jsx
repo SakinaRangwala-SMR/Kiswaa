6e23c5eefd9c417489145d84b74b902745cf164b
SakinaRangwala-SMR <sakinarangwala822m@gmail.com>
2025-11-06 23:47:25 +0530

    Update AuthContext.jsx

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -3,7 +3,7 @@ import React, { createContext } from 'react'
 export const authDataContext=createContext()
 
 function AuthContext({children}) {
-  let serverUrl="http://localhost:8000"
+  let serverUrl="https://kisswaa.onrender.com"
     let value={
     serverUrl
   }
