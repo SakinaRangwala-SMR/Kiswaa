be4493546284ac776eeef36266f17a34881f4e56
SakinaRangwala-SMR <sakinarangwala822m@gmail.com>
2025-11-07 00:08:44 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -26,7 +26,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-origin:["http://localhost:5173","http://localhost:5174"],
+origin:["https://kiswaa.onrender.com","http://localhost:5174"],
 credentials:true
 }))
 
