fd1e2748b578c59818d8fb329954d77c54e0dd09
SakinaRangwala-SMR <sakinarangwala822m@gmail.com>
2025-11-07 00:00:39 +0530

    Update Login.jsx

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -9,7 +9,7 @@ import { useContext } from 'react';
 import { authDataContext } from '../context/AuthContext';
 import axios from 'axios';
 import { signInWithPopup } from 'firebase/auth';
-import { auth, provider } from '../../utils/firebase';
+import { auth, provider } from '../../utils/Firebase.js';
 import { userDataContext } from '../context/UserContext';
 import { toast } from 'react-toastify';
 import Loading from '../component/Loading';
